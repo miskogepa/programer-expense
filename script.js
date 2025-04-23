8162f2dc42d5d469e5ccd6d8c5189582017f8c78
miskogepa <milovan.jankovic50m@gmail.com>
2025-04-23 09:32:27 +0200

    uzimanje vrednosti iz forme

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const expenseForm = document.getElementById("expense-form");
+  const expenseNameInput = document.getElementById("expense-name");
+  const expenseAmountInput = document.getElementById("expense-amount");
+  const expenseList = document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount");
+
+  let expenses = [];
+  let totalAmount = calculateTotal();
+
+  expenseForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = expenseNameInput.value.trim();
+    const amount = parseFloat(expenseAmountInput.value.trim());
+  });
+
+  function calculateTotal() {}
+});
